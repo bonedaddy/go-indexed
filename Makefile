389ccc228207a0ed1425ef7c1a9b5e4c45770099
bonedaddy <boned4ddy@protonmail.com>
2020-12-29 19:13:46 -0800

    generate bindings for all contract types

--- Makefile
+++ Makefile
@@ -7,4 +7,10 @@ copy-abi:
 gen-bindings:
 	if [ ! -d bindings ]; then mkdir bindings ; fi
 	if [ ! -d bindings/pool ]; then mkdir bindings/pool ; fi
+	if [ ! -d bindings/pair ]; then mkdir bindings/pair ; fi
+	if [ ! -d bindings/staking_rewards ]; then mkdir bindings/staking_rewards ; fi
+	if [ ! -d bindings/uniswapv2_oracle ]; then mkdir bindings/uniswapv2_oracle ; fi
 	abigen --pkg poolbindings --out bindings/pool/bindings.go
+	abigen --pkg pairbindings --out bindings/pair/bindings.go
+	abigen --pkg stakingbindings --out bindings/staking_rewards/bindings.go
+	abigen --pkg uv2oraclebindings --out bindings/uniswapv2_oracle/bindings.go
---
+++ bindings/uniswapv2_oracle/bindings.go
@@ -0,0 +1,27 @@
+// Code generated - DO NOT EDIT.
+// This file is a generated binding and any manual changes will be lost.
+
+package uv2oraclebindings
+
+import (
+	"math/big"
+	"strings"
+
+	ethereum "github.com/ethereum/go-ethereum"
+	"github.com/ethereum/go-ethereum/accounts/abi"
+	"github.com/ethereum/go-ethereum/accounts/abi/bind"
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/core/types"
+	"github.com/ethereum/go-ethereum/event"
+)
+
+// Reference imports to suppress errors if they are not otherwise used.
+var (
+	_ = big.NewInt
+	_ = strings.NewReader
+	_ = ethereum.NotFound
+	_ = bind.Bind
+	_ = common.Big1
+	_ = types.BloomLookup
+	_ = event.NewSubscription
+)
