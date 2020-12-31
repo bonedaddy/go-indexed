46d07fbe76b65b7e2635aa776055ec03e4311e8b
bonedaddy <boned4ddy@protonmail.com>
2020-12-31 14:16:21 -0800

    discord: update help menu

--- discord/init.go
+++ discord/init.go
@@ -5,7 +5,7 @@ import "github.com/bwmarrin/discordgo"
 func init() {
 	helpEmbed = &discordgo.MessageEmbed{
 		Title:       "Indexed Finance Bot Help Menu",
-		Description: "NDXBot is an unofficial Discord Bot for making read-only calls to Indexed Finance smart contracts.\n\nAll commands must be invoked with !ndx <cmd>\n\nAnything with <..> after command name expects an argument\n\nAnything with [..] after command name is an optional argument\n\nExample (stake-earned)\n!ndx stake-earned defi5 0x5a361A1dfd52538A158e352d21B5b622360a7C13\n\nExample (pool-balance)\n!ndx pool-balance defi5 0x5a361A1dfd52538A158e352d21B5b622360a7C13\n\nAll available commands are listed below",
+		Description: "NDXBot is an unofficial Discord Bot for making read-only calls to Indexed Finance smart contracts. **NDXBot will never contact you asking for your seed phrases, private keys, or other personal/sensitive information**\n\nAll commands must be invoked with !ndx <cmd>\n\nAnything with <..> after command name expects an argument\n\nAnything with [..] after command name is an optional argument\n\nExample (stake-earned)\n!ndx stake-earned defi5 0x5a361A1dfd52538A158e352d21B5b622360a7C13\n\nExample (pool-balance)\n!ndx pool-balance defi5 0x5a361A1dfd52538A158e352d21B5b622360a7C13\n\nPlease report all issues to [github](https://github.com/bonedaddy/go-indexed)\n\nAll available commands are listed below",
 		Thumbnail: &discordgo.MessageEmbedThumbnail{
 			URL: "https://pbs.twimg.com/profile_images/1342395531318976518/kIv5abLc_400x400.jpg",
 		},
