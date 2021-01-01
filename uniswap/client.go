02ab7015e53d608a394efe89625470b54eade7ad
bonedaddy <boned4ddy@protonmail.com>
2021-01-01 13:39:17 -0800

    uniswap: add monkey fix for abigen induced panics

--- uniswap/client.go
+++ uniswap/client.go
@@ -19,6 +19,7 @@ package uniswap
 import (
 	"context"
 	"errors"
+	"log"
 	"math/big"
 	"time"
 
@@ -41,7 +42,7 @@ func NewClient(bc *ethclient.Client) *Client {
 }
 
 // GetReserves retursn the available reserves in a pair
-func (c *Client) GetReserves(token0, token1 common.Address) (*Reserve, error) {
+func (c *Client) GetReserves(token0, token1 common.Address) (rsrv *Reserve, err error) {
 	addr := GeneratePairAddress(token0, token1)
 	caller, err := uniswapv2pair.NewUniswapv2pairCaller(addr, c.bc)
 	if err != nil {
@@ -49,21 +50,33 @@ func (c *Client) GetReserves(token0, token1 common.Address) (*Reserve, error) {
 	}
 	ctx, cancel := context.WithTimeout(context.Background(), time.Second*10)
 	defer cancel()
-	reserves, err := caller.GetReserves(&bind.CallOpts{
-		Context: ctx,
-	})
-	if err != nil {
-		return nil, err
-	}
-	// This is the tricky bit.
-	// The reserve call returns the reserves for token0 and token1 in a sorted order.
-	// This means we need to check if our token addresses are sorted or not and flip the reserves if they are not sorted.
-	stoken0, _ := sortAddressess(token0, token1)
-	if stoken0 != token0 {
-		// We're not sorted, so the reserves need to be flipped to represent the actual reserves.
-		reserves.Reserve0, reserves.Reserve1 = reserves.Reserve1, reserves.Reserve0
-	}
-	return &Reserve{Reserve0: reserves.Reserve0, Reserve1: reserves.Reserve1, BlockTimestampLast: reserves.BlockTimestampLast}, nil
+	// hacky workaround for https://github.com/ethereum/go-ethereum/issues/22100
+	// this allows us to save the panicking goroutine
+	defer func() {
+		reserves := struct {
+			Reserve0           *big.Int
+			Reserve1           *big.Int
+			BlockTimestampLast uint32
+		}{nil, nil, 0}
+		defer recover()
+		reserves, err = caller.GetReserves(&bind.CallOpts{
+			Context: ctx,
+		})
+		if err != nil {
+			log.Println("failed to get reserves: ", err)
+			return
+		}
+		// This is the tricky bit.
+		// The reserve call returns the reserves for token0 and token1 in a sorted order.
+		// This means we need to check if our token addresses are sorted or not and flip the reserves if they are not sorted.
+		stoken0, _ := sortAddressess(token0, token1)
+		if stoken0 != token0 {
+			// We're not sorted, so the reserves need to be flipped to represent the actual reserves.
+			reserves.Reserve0, reserves.Reserve1 = reserves.Reserve1, reserves.Reserve0
+		}
+		rsrv = &Reserve{Reserve0: reserves.Reserve0, Reserve1: reserves.Reserve1, BlockTimestampLast: reserves.BlockTimestampLast}
+	}()
+	return
 }
 
 // GetExchangeAmount returns the amount of tokens you'd receive when exchanging the given amount of token0 to token1.
