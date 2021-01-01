ecfb855dcdce7aac1ac3680f61f4082221b6ce43
bonedaddy <boned4ddy@protonmail.com>
2020-12-31 16:07:50 -0800

    discord: migrate uniswap commands

--- discord/discord.go
+++ discord/discord.go
@@ -3,7 +3,6 @@ package discord
 import (
 	"fmt"
 	"log"
-	"strings"
 
 	"github.com/bonedaddy/dgc"
 	"github.com/bonedaddy/go-indexed/bclient"
@@ -35,6 +34,9 @@ func NewClient(token string, bc *bclient.Client) (*Client, error) {
 		Prefixes:         []string{"!ndx"},
 		IgnorePrefixCase: true,  // allow any case of the prefix
 		BotsAllowed:      false, // do not allow bots
+		PingHandler: func(ctx *dgc.Ctx) {
+			ctx.RespondText("ping")
+		},
 	})
 
 	client := &Client{token: token, s: dg, bc: bc, r: router}
@@ -78,6 +80,25 @@ func NewClient(token string, bc *bclient.Client) (*Client, error) {
 		Handler:     client.stakeEarnedHandler,
 	})
 
+	router.RegisterCmd(&dgc.Command{
+		Name:        "uniswap",
+		Description: "command group for interacting with Indexed uniswap related contracts",
+		Usage:       " uniswap <subcommand> <args...>",
+		Example:     " uniswap exchange-amount eth-defi5 1.0",
+		SubCommands: []*dgc.Command{
+			&dgc.Command{
+				Name:        "exchange-amount",
+				Description: "returns the amount of tokens received in exchanged for the given pair. \"!ndx uniswap exchange-amount defi5-eth 1.0\" will return the amount of ETH received for swapping 1 DEFI5 to ETH, whereas \"!ndx uniswap exchange-amount eth-defi5 1.0\" will return the amount of DEFI5 received for swapping 1 ETH to DEFI5",
+				Usage:       " uniswap exchange-amount <direction> <amount>",
+				Example:     " uniswap exchange-amount eth-defi5 1.0\n!ndx uniswap exchange-amount defi5-eth 1.0",
+				Handler:     client.uniswapExchangeAmountHandler,
+			},
+		},
+		Handler: func(ctx *dgc.Ctx) {
+			ctx.RespondText("invalid invocation please run a specific subcommand")
+		},
+	})
+
 	router.Initialize(dg)
 
 	// client.s.AddHandler(client.messageCreate)
@@ -93,54 +114,9 @@ func (c *Client) Close() error {
 	return c.s.Close()
 }
 
-// handleCommand processes incoming messages from discord and determines what to do with them
-func (c *Client) handleCommand(s *discordgo.Session, m *discordgo.MessageCreate, args []string) {
-	if len(args) == 1 && args[0] == "!ndx" {
-		c.sendHelp(s, m)
-		return
-	} else if len(args) == 2 && args[0] == "!ndx" && args[1] == "help" {
-		c.sendHelp(s, m)
-		return
-	}
-
-	if len(args) >= 2 {
-		if args[1] == "notify" {
-			c.handleNotif(s, m, args)
-			return
-		}
-		if args[1] == "uniswap" {
-			c.handleUniswap(s, m, args)
-			return
-		}
-	}
-	c.s.ChannelMessageSend(m.ChannelID, "invalid command invocation")
-}
-
 func (c *Client) sendHelp(s *discordgo.Session, m *discordgo.MessageCreate) {
 	if _, err := s.ChannelMessageSendEmbed(m.ChannelID, helpEmbed); err != nil {
 		fmt.Println("error sending message: ", err.Error())
 		return
 	}
-}
-
-// This function will be called (due to AddHandler above) every time a new
-// message is created on any channel that the autenticated bot has access to.
-func (c *Client) messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
-	// Ignore all messages created by the bot itself
-	// This isn't required in this specific example but it's a good practice.
-	if m.Author.ID == s.State.User.ID {
-		return
-	}
-
-	// parse the message contents based off string fields
-	args := strings.Fields(m.Content)
-	if len(args) == 0 {
-		return
-	}
-	// ensure the first field is a valid invocation of index bot1
-	if args[0] != "!ndx" {
-		fmt.Println("invalid invocation")
-		return
-	}
-	c.handleCommand(s, m, args)
 }
