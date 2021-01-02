37659ee2e0096491078d7ec6c9ec6981a7dc123d
bonedaddy <boned4ddy@protonmail.com>
2021-01-02 14:03:30 -0800

    fix build tooling

--- .scripts/release.sh
+++ .scripts/release.sh
@@ -10,4 +10,9 @@ go build -o release/gondx -ldflags "-X main.Version=$VERSION" ./cmd/gondx
 
 docker build --build-arg VERSION=$VERSION -t bonedaddy/gondx:$VERSION .
 
-docker image save bonedaddy/gondx:$VERSION --output gondx_$VERSION.tar
+docker image save bonedaddy/gondx:$VERSION --output release/gondx-docker_$VERSION.tar
+
+ls ./release/* > files
+for i in $(cat files); do
+    sha256sum "$i" > "$i.sha256"
+done
