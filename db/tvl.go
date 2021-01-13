e44ec26cac5835d7e9c3c56bb7d0bd36b2769f17
bonedaddy <boned4ddy@protonmail.com>
2021-01-13 15:27:25 -0800

    db: add tests to retrieve value locked

---
+++ db/tvl.go
@@ -0,0 +1,25 @@
+package db
+
+import "gorm.io/gorm"
+
+// TotalValueLocked records the USD value of funds locked within index pool
+type TotalValueLocked struct {
+	gorm.Model
+	PoolName    string
+	ValueLocked float64
+}
+
+// RecordValueLocked records the USD value locked into a pool
+func (d *Database) RecordValueLocked(pool string, locked float64) error {
+	return d.db.Create(&TotalValueLocked{PoolName: pool, ValueLocked: locked}).Error
+}
+
+// LastValueLocked returns the latest record total value locked entry
+func (d *Database) LastValueLocked(pool string) (float64, error) {
+	var tvl TotalValueLocked
+	if err := d.db.Model(&TotalValueLocked{}).Where(
+		"pool_name = ?", pool).Last(&tvl).Error; err != nil {
+		return 0, err
+	}
+	return tvl.ValueLocked, nil
+}
