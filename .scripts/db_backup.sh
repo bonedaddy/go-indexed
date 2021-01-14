b7992f67ab269cd7b93875c4d8bc0755760b7609
bonedaddy <boned4ddy@protonmail.com>
2021-01-13 16:54:44 -0800

    update backup script to enable copying to remote host

--- .scripts/db_backup.sh
+++ .scripts/db_backup.sh
@@ -2,13 +2,26 @@
 
 # used to backup the postgresql database
 
+MC_COPY="yes"
+MC_INSECURE="yes"
+MC_HOST="..."
+MC_BUCKET="indexed-db-backups"
+FILENAME=dump_`date +%d-%m-%Y"_"%H_%M_%S`.sql
+
 if [[ ! -d db_backups ]]; then
     mkdir db_backups
 fi
-
-FILENAME=dump_`date +%d-%m-%Y"_"%H_%M_%S`.sql
 
 # https://stackoverflow.com/questions/24718706/backup-restore-a-dockerized-postgresql-database
 docker-compose exec -T postgres pg_dumpall -c -U postgres > "db_backups/$FILENAME"
 
 gzip -9 "db_backups/$FILENAME"
+
+# used to copy backups to remote minio host
+if [[ "$MC_COPY" == "yes" ]]; then
+    if [[ "$MC_INSECURE" == "yes" ]]; then
+        mc cp --insecure "db_backups/$FILENAME.gz" "$MC_HOST/$MC_BUCKET"
+    else
+        mc cp "db_backups/$FILENAME.gz" "$MC_HOST/$MC_BUCKET"
+    fi
+fi
