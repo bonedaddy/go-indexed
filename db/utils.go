98de07c1a4bac065495fb2f503c743864642089d
bonedaddy <boned4ddy@protonmail.com>
2021-01-15 14:18:26 -0800

    db: remove index

--- db/utils.go
+++ db/utils.go
@@ -9,7 +9,7 @@ import (
 // Model is a copy of the gorm model type but using slightly different configurations
 type Model struct {
 	ID        uint      `gorm:"primarykey"`
-	CreatedAt time.Time `gorm:"index"` // this is changed to include the index
+	CreatedAt time.Time // `gorm:"index"` // this is changed to include the index
 	UpdatedAt time.Time
 	DeletedAt gorm.DeletedAt `gorm:"index"`
 }
