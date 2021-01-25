b11ea25dd57b59e8d553909685c183e366edd186
bonedaddy <boned4ddy@protonmail.com>
2021-01-24 18:38:57 -0800

    fix aave price calculation

--- bclient/pool.go
+++ bclient/pool.go
@@ -1,6 +1,7 @@
 package bclient
 
 import (
+	"log"
 	"math/big"
 	"strings"
 
@@ -70,7 +71,7 @@ func (c *Client) GetTotalValueLocked(ip IndexPool) (float64, error) {
 	for symbol, addr := range tokens {
 		// hard coded list for alternate price lookups of X->ETH->DAI
 		switch strings.ToLower(symbol) {
-		case "crv", "uma", "omg", "yfi":
+		case "crv", "uma", "omg", "yfi", "aave":
 			erc, err := erc20.NewErc20(addr, c.ec)
 			if err != nil {
 				return 0, errors.Wrap(err, "failed to get erc20 contract")
@@ -101,6 +102,7 @@ func (c *Client) GetTotalValueLocked(ip IndexPool) (float64, error) {
 				tokenUsdValue: edF * tdF,
 				tokenAddress:  addr,
 			}
+			log.Printf("%s USD value: %0.2f", symbol, values[symbol].tokenUsdValue)
 		default:
 			// calculate the reserves
 			reserves, err := uc.GetReserves(addr, DAITokenAddress)
@@ -114,6 +116,7 @@ func (c *Client) GetTotalValueLocked(ip IndexPool) (float64, error) {
 				tokenUsdValue: priceF,
 				tokenAddress:  addr,
 			}
+			log.Printf("%s USD value: %0.2f", symbol, values[symbol].tokenUsdValue)
 		}
 	}
 	var totalValueUSD float64
